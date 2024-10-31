d5d81191b9ff15716030adce847dc24a594c6d94
Gaurang <118437476+gaurang2711@users.noreply.github.com>
2024-10-31 14:04:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const tocLinks=document.querySelectorAll(".stackedit__toc a");function toggleDarkMode(){document.body.classList.toggle("dark-mode"),document.body.classList.contains("dark-mode")?localStorage.setItem("darkMode","enabled"):localStorage.removeItem("darkMode")}tocLinks.forEach(e=>{e.addEventListener("click",function(e){e.preventDefault();let t=this.getAttribute("href"),o=document.querySelector(t);o&&o.scrollIntoView({behavior:"smooth"})})}),window.onload=()=>{"enabled"===localStorage.getItem("darkMode")&&document.body.classList.add("dark-mode")};
+const icon = document.querySelector('.dark-mode-button');
+const tocLinks = document.querySelectorAll(".stackedit__toc a");
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("darkMode", "enabled");
+        icon.src = "resources/lm.svg";
+    } else {
+        localStorage.removeItem("darkMode");
+        icon.src = "resources/dm.svg";
+    }
+}
+
+tocLinks.forEach(e => {
+    e.addEventListener("click", function(e) {
+        e.preventDefault();
+        let t = this.getAttribute("href"),
+            o = document.querySelector(t);
+        o && o.scrollIntoView({ behavior: "smooth" })
+    })
+}), window.onload = () => {
+    "enabled" === localStorage.getItem("darkMode") && document.body.classList.add("dark-mode");
+};
