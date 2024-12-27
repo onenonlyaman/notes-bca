1d02122f5d993ec9dbca6de8101ee4e83e770db2
Gaurang <gauraangdalal@gmail.com>
2024-12-27 13:31:31 +0530

    evs

--- EVSchp1/script.js
+++ EVSchp1/script.js
@@ -61,3 +61,30 @@ window.onload = () => {
     const userName = getUserName();
     greetingElement.textContent = `Hello, ${userName}! `;
 };
+
+const download_button =
+document.getElementById('download_Btn');
+const content =
+document.getElementById('content');
+
+download_button.addEventListener
+('click', async function () {
+    const filename = 'Chapter 1: Human and Environment.pdf';
+
+    try {
+        const opt = {
+            margin: 1,
+            filename: filename,
+            image: { type: 'jpeg', quality: 0.98 },
+            html2canvas: { scale: 2 },
+            jsPDF: {
+                unit: 'in', format: 'letter',
+                orientation: 'portrait'
+            }
+        };
+        await html2pdf().set(opt).
+            from(content).save();
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+});
